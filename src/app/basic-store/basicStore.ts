431db3a037e7c5c641592c4ebb9068afa4d3f832
CSIDanielD <daniel.durbin@csiweb.com>
2021-04-05 16:55:08 -0500

    changed basicStore slightly and added some JsDoc comments to its methods

--- src/app/basic-store/basicStore.ts
+++ src/app/basic-store/basicStore.ts
@@ -1,7 +1,7 @@
 import { BehaviorSubject, Subject } from "rxjs";
 import { map } from "rxjs/operators";
 import { Action } from "./action";
-import { withState } from "./actionContext";
+import { createActionContext, withState } from "./actionContext";
 import { ReducerMap } from "./reducer";
 import { Selector } from "./selector";
 import {
@@ -16,6 +16,11 @@ export class BasicStore<S, R extends ReducerMap<S, any>> {
   >;
   protected _dispatcher = new Subject<Action<any>>();
 
+  /**
+   * A convenience object containing every action key mapped to its action creator.
+   * @use Use the object destructuring syntax to extract whichever registered action(s) you
+   * need like this: `const { actionA, actionB } = store.actions;`
+   */
   get actions() {
     const actionCreators = Object.entries(this._actionReducers.value).reduce(
       (map, entry) => {
@@ -31,31 +36,55 @@ export class BasicStore<S, R extends ReducerMap<S, any>> {
     >;
   }
 
+  /**
+   * Select all or a part of the current state value synchronously.
+   * @param selector The selector that will be called with the current state value.
+   */
   select<T>(selector: Selector<S, T>) {
     return selector({ ...this._state.value });
   }
 
+  /**
+   * Select all or a part of the current state value as an Observable. Useful if you want to use your
+   * selected state in a display component or async logic.
+   * @param selector The selector that will be supplied to the RxJs `map` operator.
+   */
   selectAsync<T>(selector: Selector<S, T>) {
     return this._state.asObservable().pipe(map(selector));
   }
 
-  // TODO: Type check could probably be changed to only allow actions that are in _actionReducers
+  /**
+   * Dispatch an action to update the current state. This is the only way to update the state's value.
+   * @param action The action to dispatch. The action's 'type' string must match one of the registered reducers.
+   */
   dispatch<A extends Action<any>>(action: A) {
-    if (!this._actionReducers[action.type]) {
+    // TODO: Type check could probably be changed to only allow actions that are in _actionReducers
+    if (!this._actionReducers.value[action.type]) {
       throw new Error(`No action registered with type '${action.type}'!`);
     }
 
     this._dispatcher.next(action);
   }
 
-  protected _commitAction<A extends Action<any>>(action: A) {
-    console.log("Action dispatched:", action);
+  /**
+   * Dispatches the action to its registered reducer and updates the current state with
+   * the reducer's returned value.
+   */
+  protected async _commitAction<A extends Action<any>>(action: A) {
+    const { reducer } = this._actionReducers.value[action.type];
+
+    const stateFn = () => this.select(s => s);
+    const evaluatedState = new Promise<S>((resolve, reject) => {
+      return resolve(reducer(stateFn, action.payload));
+    });
+
+    this._state.next(await evaluatedState);
   }
 
-  constructor(initialState: S, reducers: R) {
+  constructor(initialState: S, reducers: R, storeName: string = "store") {
     this._state = new BehaviorSubject(initialState);
     this._actionReducers = new BehaviorSubject(
-      withState<S>().createActionReducerMap(reducers)
+      createActionContext<S>(storeName).createActionReducerMap(reducers)
     );
 
     // Automatically commit actions to mutate the state
--- src/app/store/groceryState.ts
+++ src/app/store/groceryState.ts
@@ -1,5 +1,3 @@
-import { createActionContext } from "../basic-store/actionContext";
-import { BasicStore } from "../basic-store/basicStore";
 import { Food } from "../types/food";
 import { Supply } from "../types/supply";
 
@@ -13,41 +11,51 @@ export const defaultState: GroceryState = {
   supplies: []
 };
 
-const supplyActions = createActionContext<GroceryState>(
-  "supplyActions"
-).createReducerMap({
-  addSupply: (getState, payload: { supply: Supply }) => {
-    const state = getState();
-    state.supplies.push(payload.supply);
-    return state;
-  }
-});
+// const supplyActions = createActionContext<GroceryState>(
+//   "supplyActions"
+// ).createReducerMap({
+//   addSupply: (getState, payload: { supply: Supply }) => {
+//     const state = getState();
+//     state.supplies.push(payload.supply);
+//     return state;
+//   }
+// });
 
-const foodActions = createActionContext<GroceryState>(
-  "foodActions"
-).createReducerMap({
-  addFood: (getState, payload: { food: Food }) => {
-    const state = getState();
-    state.foods.push(payload.food);
-    return state;
-  }
-});
+// const foodActions = createActionContext<GroceryState>(
+//   "foodActions"
+// ).createReducerMap({
+//   addFood: (getState, payload: { food: Food }) => {
+//     const state = getState();
+//     state.foods.push(payload.food);
+//     return state;
+//   }
+// });
 
-// Here's how to combine the actions objects into a single object that has all actions (an intersection type).
-// You MUST precede all actions with the spread operator (...) for their properties to properly map to the object 
-const allActions = {
-  ...supplyActions,
-  ...foodActions
-};
+// // Here's how to combine the actions objects into a single object that has all actions (an intersection type).
+// // You MUST precede all actions with the spread operator (...) for their properties to properly map to the object.
+// const allActions = {
+//   ...supplyActions,
+//   ...foodActions
+// };
 
-const testStore = new BasicStore(defaultState, allActions);
-const { addSupply, addFood } = testStore.actions;
-const addPaperTowel = addSupply({
-  supply: {
-    id: 5,
-    name: "Paper Towel",
-    price: 7.99
-  }
-});
+// const testStore = new BasicStore(defaultState, allActions);
+// const { addSupply, addFood } = testStore.actions;
+// const addPaperTowel = addSupply({
+//   supply: {
+//     id: 5,
+//     name: "Paper Towel",
+//     price: 7.99
+//   }
+// });
 
-testStore.dispatch(addPaperTowel);
+// testStore.dispatch(addPaperTowel);
+
+// const x = { color: "red" };
+// const y = { value: 3 };
+
+// const xy = { ...x, ...y }; // This does what I want.
+// const alt: typeof x & typeof y = {
+//   // Intersection also works.
+//   color: "blue",
+//   value: 11
+// };
